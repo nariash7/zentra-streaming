fcb6dce1d786af54a952d86f2d07986eb3f06ea4
nariash7 <nariash@autonoma.edu.pe>
2026-09-25 00:45:42 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.ZENTRA_CONFIG = {
-  SUPABASE_URL: "PEGA_AQUI_TU_URL_DE_SUPABASE",
-  SUPABASE_ANON_KEY: "PEGA_AQUI_TU_CLAVE_PUBLICA_ANON"
+  SUPABASE_URL: "https://poteocrwtanmaflwguqq.supabase.co",
+  SUPABASE_ANON_KEY: "sb_publishable_DMsaZab13JUdjP99Z6OKJA_kEGfdVV2"
 };
